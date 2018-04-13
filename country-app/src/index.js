59bd00c8d887e7d195a3a229a605c95585809f4b
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-04-13 11:58:29 +0200

    Add base files, function, and devtools

---
+++ country-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import DevTools from "./DevTools/DevTools";
+import { getCountries } from "./actions/actions-countries"
+
+render(
+    <Provider store={store}>
+     <div>
+        <DevTools />
+        <h1>Inicjalizacja projektu</h1>
+     </div>
+    </Provider>,
+    document.getElementById('root')
+);
+
+store.dispatch(getCountries());
